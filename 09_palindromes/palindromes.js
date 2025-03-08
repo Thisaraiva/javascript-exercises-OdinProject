0a9681136e394a6b716435345e3f5c9c4c00ee2c
Thisaraiva <thiagofreitassaraiva@yahoo.com.br>
2025-03-08 17:44:34 -0300

    Resolução do exercício 11 getTheTitles

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,9 @@
 const palindromes = function (...args) {
-
     return args.every(str => {
-
-        const cleanedStr = str.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
-
+        // Remove caracteres não alfanuméricos e converte para minúsculas
+        const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+        // Verifica se a string normalizada é igual à sua versão invertida
         return cleanedStr === cleanedStr.split('').reverse().join('');
-
     });
 };
 
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function (list) {
 
+    //let titles = [];
+
+    /*for (const title of list) {
+        titles.push(title.title);
+    }
+        
+    return titles;*/
+
+    return list.map(titles => titles.title);   
 };
 
 // Do not edit below this line
