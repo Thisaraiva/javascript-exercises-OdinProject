c5b183de9a3ad0f80ed624cc7aad66e5c0106e49
Thisaraiva <thiagofreitassaraiva@yahoo.com.br>
2025-03-08 15:36:59 -0300

    Execução dos exercícios dos testes 1 a 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,32 @@
-const add = function() {
+const add = function(a, b) {
+
+  return a + b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((total, num) => total + num, 0);	
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  return numbers.reduce((total, num) => total * num, 1);
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+
+  if (num < 2) return 1; 
+  else{
+    return num * factorial(num - 1);
+  }
 	
 };
 
@@ -31,3 +39,6 @@ module.exports = {
   power,
   factorial
 };
+
+
+console.log(factorial(6));
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (...args) {
 
+    return args.every(str => {
+
+        const cleanedStr = str.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
+
+        return cleanedStr === cleanedStr.split('').reverse().join('');
+
+    });
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+console.log(palindromes("caracb"));
