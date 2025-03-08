4603af39769bc67fd5233a6439c2914b29901814
Thisaraiva <thiagofreitassaraiva@yahoo.com.br>
2025-03-08 18:47:35 -0300

    Execução do exercício 12 findTheOldest e fim da tarefa de testes

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+
+    if (array.length === 0) return undefined;
+
+    const getAge = (person) => {
+        const currentYear = new Date().getFullYear();
+        const deathYear = person.yearOfDeath || currentYear;
+        return deathYear - person.yearOfBirth;
+    };
+
+    return array.reduce((oldest, current) => {                
+        const oldestAge = getAge(oldest);
+        const currentAge = getAge(current);
+        return oldestAge < currentAge ? current : oldest;
+    }, array[0]);
 
 };
 
